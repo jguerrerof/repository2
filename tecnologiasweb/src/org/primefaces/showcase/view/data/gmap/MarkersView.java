e78f3d53638325577c883bd4f981137bc1d46283
jguerrerof <43593691+jguerrerof@users.noreply.github.com>
2018-10-03 19:32:35 -0500

    jj

--- tecnologiasweb/src/org/primefaces/showcase/view/data/gmap/MarkersView.java
+++ tecnologiasweb/src/org/primefaces/showcase/view/data/gmap/MarkersView.java
@@ -19,16 +19,19 @@ public class MarkersView implements Serializable {
         simpleModel = new DefaultMapModel();
           
         //Shared coordinates
-        LatLng coord1 = new LatLng(36.879466, 30.667648);
-        LatLng coord2 = new LatLng(36.883707, 30.689216);
-        LatLng coord3 = new LatLng(36.879703, 30.706707);
-        LatLng coord4 = new LatLng(36.885233, 30.702323);
+        //LatLng coord1 = new LatLng(36.879466, 30.667648);
+        LatLng coord1 = new LatLng(-2.060220, -79.877965);        
+        LatLng coord2 = new LatLng(-2.1277843,-79.9042727);
+        LatLng coord3 = new LatLng(-2.126074, -79.897165);
+        LatLng coord4 = new LatLng(-2.146075, -79.901837); 
+      //  LatLng coord3 = new LatLng(36.879703, 30.706707);
+      //  LatLng coord4 = new LatLng(36.885233, 30.702323);
           
         //Basic marker
-        simpleModel.addOverlay(new Marker(coord1, "Central"));
-        simpleModel.addOverlay(new Marker(coord2, "Sucursal 1"));
-        simpleModel.addOverlay(new Marker(coord3, "Sucursal 2"));
-        simpleModel.addOverlay(new Marker(coord4, "Sucursal 3"));
+        simpleModel.addOverlay(new Marker(coord1, "Central - Urbanización Matices mz 6 villa 22"));
+        simpleModel.addOverlay(new Marker(coord2, "Sucursal 1 - Sauces 8 mz 8 villa 6"));
+        simpleModel.addOverlay(new Marker(coord3, "Sucursal 2 - Sauces 6 mz 7 villa 16 "));
+        simpleModel.addOverlay(new Marker(coord4, "Sucursal 3 - Urdenor mz 3 villa 21"));
     }
   
     public MapModel getSimpleModel() {
